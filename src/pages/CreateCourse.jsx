6a3d105d045b3d566b406d43d15c21357d348396
Prabhu Kalyan Korivi <prabhukalyan30@gmail.com>
2025-02-01 23:35:04 +0530

    fix import

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useCourses } from "@/lib/context/courses";
+import { useCourses } from "@/lib/context/Courses";
 import { useVideos } from "@/lib/context/Videos";
 import { useUser } from "@clerk/clerk-react";
 import axios from "axios";
